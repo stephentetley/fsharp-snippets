92a60b081ca83cf20a9bfa9773f8db24db452773
stephentetley <stephen.tetley@gmail.com>
2017-04-15 14:49:09 +0100

    Getting Fsharp to read Word docs. Simple example, to be expanded...

---
+++ fsharp-snippets/WordForm.fsx
@@ -0,0 +1,17 @@
+﻿
+#r "Microsoft.Office.Interop.Word"
+open Microsoft.Office.Interop
+
+// Run Word...
+let app = new Word.ApplicationClass()
+
+
+let filename = ref (box @"G:\work\working\Survey1.docx")
+let doc = app.Documents.Open(filename)
+
+doc.Sections 
+    |> Seq.cast<Word.Section> 
+    |> Seq.iter (fun s1 -> printfn "Tables: %i" s1.Range.Tables.Count)
+
+
+doc.Close(SaveChanges = ref (box false))
