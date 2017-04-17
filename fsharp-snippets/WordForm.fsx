542a738fdbd89f5f7b6e8e2346b861409868bcce
stephentetley <stephen.tetley@gmail.com>
2017-04-17 22:20:47 +0100

    More work with Word object model, the code is still trivial but it has still taken a fair amount of effort so seems worth checkpointing.

--- fsharp-snippets/WordForm.fsx
+++ fsharp-snippets/WordForm.fsx
@@ -3,15 +3,34 @@
 open Microsoft.Office.Interop
 
 // Run Word...
-let app = new Word.ApplicationClass()
-
-
+let app = new Word.ApplicationClass ()
 let filename = ref (box @"G:\work\working\Survey1.docx")
 let doc = app.Documents.Open(filename)
+
+
+printfn "Sections: %i" doc.Sections.Count
+printfn "Paragraphs: %i" doc.Paragraphs.Count
+printfn "Tables: %i" doc.Tables.Count
+
+
+let showTable (t1 : Word.Table) = 
+    let ans = t1.ConvertToText(ref (box Word.WdSeparatorType.wdSeparatorHyphen))
+    printfn "Table: %s" ans.Text
+
+let add1 i = i + 1
+
+doc.Tables 
+    |> Seq.cast<Word.Table> 
+    |> Seq.iter showTable
+
 
 doc.Sections 
     |> Seq.cast<Word.Section> 
     |> Seq.iter (fun s1 -> printfn "Tables: %i" s1.Range.Tables.Count)
 
+let all : Word.Range = doc.Content
+all.Select()
+printfn "Characters: %i" all.Characters.Count
 
 doc.Close(SaveChanges = ref (box false))
+app.Quit()
