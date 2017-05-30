cced7c7945eab8aff1ea2b9382d837cf20307d1a
stephentetley <stephen.tetley@gmail.com>
2017-05-30 14:15:03 +0100

    Initial commit of a utility to generate a Doc / PDF from a list of photos.

---
+++ PhotoDoc/src/DocBuilder.fs
@@ -0,0 +1,12 @@
+﻿
+[<AutoOpen>]
+module PhotoDoc.DocBuilder
+
+// Add a reference via the COM tab 
+// All that PIA stuff is outdated for Office 365 / .Net 4.5 / VS2015 
+open Microsoft.Office.Interop
+
+
+type DocBuilder =
+    { RngLast : Word.Range }
+
---
+++ PhotoDoc/src/Script.fsx
@@ -0,0 +1,24 @@
+﻿
+#r "Microsoft.Office.Interop.Word"
+
+#load "DocBuilder.fs"
+
+
+open System.IO
+
+let dir1 = @"G:\work\photos1\site photos"
+
+
+Directory.Exists(dir1)
+
+Directory.EnumerateDirectories(dir1) |> Seq.iter (fun dir -> printfn "%s" dir)
+
+Directory.EnumerateFiles(dir1) |> Seq.iter (printfn "%s")
+
+let rec allFiles dir = 
+    seq { for file in Directory.GetFiles dir do
+            yield file
+          for subdir in Directory.GetDirectories dir do
+            yield! allFiles subdir }
+
+allFiles dir1 |> Seq.iter (printfn "%s")
