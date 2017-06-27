faea1da0ce6b8e2b5b24a3e20e25a73db755c6be
stephentetley <stephen.tetley@gmail.com>
2017-06-27 22:39:25 +0100

    FilePaths - added a slow recursive implementation of Levenshtein edit difference. The idea being to approxiamately match names in the file system. To be useful I'll need a faster implementation and possibly a way of globbing names.

---
+++ FilePaths/src/Test2.fsx
@@ -0,0 +1,10 @@
+﻿#load "Utils.fs"
+
+open FilePath.Utils
+
+let test01 () = slowLevenshtein "history" "mystery"
+
+// Should be 3 (this is Wikipedia's example...)
+let test02 () = slowLevenshtein "kitten" "sitting"
+
+let test03 () = slowLevenshtein "very equal" "very equal"
---
+++ FilePaths/src/Utils.fs
@@ -0,0 +1,15 @@
+﻿[<AutoOpen>]
+module FilePath.Utils
+
+let slowLevenshtein (s:string) (t:string) : int =
+    let rec lev (slen:int) (tlen:int) = 
+        if slen = 0 then tlen
+        else if tlen = 0 then slen
+        else let cost = if s.[slen-1] = t.[tlen-1] then 0 else 1
+             let a = 1 + lev (slen-1) tlen
+             let b = 1 + lev slen (tlen-1)
+             let c = cost + lev (slen-1) (tlen-1)
+             min a (min b c)
+    lev (s.Length) (t.Length)
+
+
