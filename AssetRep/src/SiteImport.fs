6db2578495fe2f9ba01d2cd48b5110a7ba1c4096
stephentetley <stephen.tetley@gmail.com>
2017-07-01 21:47:55 +0100

    AssetRep - new mini-porject to import data from from Excel into a SQLite database.

---
+++ AssetRep/src/SiteImport.fs
@@ -0,0 +1,47 @@
+﻿
+
+[<AutoOpen>]
+module AssetRep.SiteImport
+
+open System.IO
+open System.Data
+open System.Data.SQLite
+
+let makeConn (loc:string) : Option<SQLiteConnection> = 
+    if File.Exists(loc) then
+        let args = sprintf "Data Source=\"%s\";Version=3;" loc
+        let conn = new SQLiteConnection(args)
+        // conn.Open ()
+        Some(conn)
+    else 
+        printfn "Database file not found '%s'" loc
+        None
+
+let withOpenConn (conn:SQLiteConnection) (fn : SQLiteConnection -> 'a) : 'a = 
+    conn.Open ()
+    let ans = fn conn
+    conn.Close ()
+    ans
+
+
+
+let testConn (conn:SQLiteConnection) : unit =  
+    let query = "SELECT * FROM sites;"
+    let command = new SQLiteCommand(query, conn)
+    let reader : SQLiteDataReader = command.ExecuteReader()
+    while reader.Read() do
+        printfn "%s %s" (reader.GetString(0)) (reader.GetString(2))
+
+let deleteData (conn:SQLiteConnection) : int =  
+    let query = "DELETE FROM sites;"
+    let (command : SQLiteCommand) = new SQLiteCommand(query, conn)
+    command.ExecuteNonQuery ()
+
+
+
+let insertDummy (conn:SQLiteConnection) = 
+    let query = "INSERT INTO sites (sai_number, postal_address1, post_code) VALUES ('SAI00482900', 'YORKSTONE CLOSE', 'S2 5DN');"
+    let (command : SQLiteCommand) = new SQLiteCommand(query, conn)
+    command.ExecuteNonQuery ()
+
+
---
+++ AssetRep/src/Test1.fsx
@@ -0,0 +1,47 @@
+﻿
+#I @"..\packages\System.Data.SQLite.Core.1.0.105.2\lib\net46"
+#r "System.Data.SQLite"
+
+#I @"..\packages\FSharpx.Extras.2.2.1\lib\net45"
+#I @"..\packages\FSharpx.Collections.1.17.0\lib\net40"
+#r "FSharpx.Collections.dll"
+#r "FSharpx.Extras.dll"
+
+#I @"..\packages\ExcelProvider.0.8.2\lib"
+#r "ExcelProvider.dll"
+
+
+#load "SiteImport.fs"
+
+
+open System
+open System.Data.SQLite
+open FSharpx
+open FSharp.ExcelProvider
+
+open AssetRep.SiteImport
+
+
+let test01 () =
+    let loc = @"E:\\coding\\fsharp\\assetrep\\data\\assetrep.db"
+    let dbconn = makeConn(loc) 
+    dbconn.Do (fun conn -> withOpenConn conn (fun oc -> let _ = deleteData oc
+                                                        let _ = insertDummy oc
+                                                        testConn oc))
+
+
+type Data1Row = ExcelFile< @"G:\\work\\NEXT_GEN_SURVEYS\\Supplied Info\\AI Data.xlsx",
+                            SheetName = "qInstallationAddressesAndRespOf",
+                            ForceString = true >
+
+
+let test02 () = 
+    let file = new Data1Row()
+    for rowi in file.Data do
+        printfn "%s %s %s" rowi.SiteReference rowi.``Postal Address 1`` rowi.``Post Code``
+
+
+
+
+
+
