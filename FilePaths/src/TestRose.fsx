66f85d0b2d2532bd853c68e00a3265471001c3cd
stephentetley <stephen.tetley@gmail.com>
2017-07-06 22:37:30 +0100

    FilePaths - added a general RoseTree. It is anticipated to be useful as a transitory representation.

---
+++ FilePaths/src/TestRose.fsx
@@ -0,0 +1,14 @@
+﻿#load "RoseTree.fs"
+
+open FilePaths
+
+let tree1 : RoseTree.Tree<string> = 
+    RoseTree.Tree("root", [ RoseTree.Tree("branch1", [])
+                          ; RoseTree.Tree("branch2", [])
+                          ])
+
+
+
+let test01 () = printfn "%s" <| RoseTree.drawFlat id "\\" tree1
+
+
