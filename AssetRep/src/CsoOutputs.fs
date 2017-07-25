ea83b822bd3504b8430ed210fb08307c0285a42b
stephentetley <stephen.tetley@gmail.com>
2017-07-25 22:02:15 +0100

    AssetRep - added a Excel table reading bulk import sample.

---
+++ AssetRep/src/CsoOutputs.fs
@@ -0,0 +1,68 @@
+﻿[<AutoOpen>]
+module AssetRep.CsoOutputs
+
+open System.IO
+open System.Data
+open System.Data.SQLite
+open FSharp.ExcelProvider
+
+let escapeValueText (s:string) : string = 
+    let escape (s1 :string) = s1.Replace("'", "''")
+    match s with null -> "" | _ -> escape s
+
+
+let makeConn (loc:string) : Option<SQLiteConnection> = 
+    if File.Exists(loc) then
+        let args = sprintf "Data Source=\"%s\";Version=3;" loc
+        let conn = new SQLiteConnection(args)
+        // conn.Open ()
+        Some(conn)
+    else 
+        printfn "Database file not found '%s'" loc
+        None
+
+let withOpenConn (conn:SQLiteConnection) (fn : SQLiteConnection -> 'a) : 'a = 
+    conn.Open ()
+    let ans = fn conn
+    conn.Close ()
+    ans
+
+
+let runNonQuery (conn:SQLiteConnection) (statement:string) : unit = 
+    let (command : SQLiteCommand) = new SQLiteCommand(statement, conn)
+    ignore <| command.ExecuteNonQuery ()
+
+type UltrasonicsRow = ExcelFile< @"G:\work\CSO_Outputs\ultrasonics.xlsx",
+                                    SheetName = "Sheet1",
+                                    ForceString = true>
+
+let insertUltrasonicsRow (conn:SQLiteConnection) (row1:UltrasonicsRow.Row) : unit = 
+    let stmt = sprintf "INSERT INTO ultrasonics (reference, \
+                                           common_name, \
+                                           site_name, \
+                                           installed_from, \
+	                                       manufacturer, \
+                                           model, \
+                                           asset_status ) \
+                         VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s');"
+                         row1.Reference
+                         (escapeValueText <| row1.``Common Name``)
+                         (escapeValueText <| "TODO")
+                         (escapeValueText <| row1.``Installed From``)
+                         (escapeValueText <| row1.Manufacturer)
+                         (escapeValueText <| row1.Model)
+                         (escapeValueText <| row1.AssetStatus)
+    runNonQuery conn stmt
+
+let insertAllUltrasonics (conn:SQLiteConnection) = 
+    let file = new UltrasonicsRow()
+    let trans = conn.BeginTransaction(System.Data.IsolationLevel.ReadCommitted)
+    for rowi in file.Data do
+        insertUltrasonicsRow conn rowi
+        printfn "%s %s" rowi.Reference  rowi.``Common Name``
+    trans.Commit ()
+
+let deleteAllUltrasonics (conn:SQLiteConnection) : int =  
+    let query = sprintf "DELETE FROM ultrasonics;"
+    let (command : SQLiteCommand) = new SQLiteCommand(query, conn)
+    command.ExecuteNonQuery ()
---
+++ AssetRep/src/TestCso.fsx
@@ -0,0 +1,32 @@
+﻿
+#I @"..\packages\System.Data.SQLite.Core.1.0.105.2\lib\net46"
+#r "System.Data.SQLite"
+
+#I @"..\packages\FSharpx.Extras.2.2.1\lib\net45"
+#I @"..\packages\FSharpx.Collections.1.17.0\lib\net40"
+#r "FSharpx.Collections.dll"
+#r "FSharpx.Extras.dll"
+
+#I @"..\packages\ExcelProvider.0.8.2\lib"
+#r "ExcelProvider.dll"
+
+
+#load "CsoOutputs.fs"
+
+
+open System
+open System.Data.SQLite
+open FSharpx
+open FSharp.ExcelProvider
+
+open AssetRep.CsoOutputs
+
+
+
+let test01 () =
+    let loc = System.IO.Path.Combine(__SOURCE_DIRECTORY__, "..", @"data/cso_outputs.db")
+    printfn "%s" loc
+    let dbconn = makeConn(loc) 
+    dbconn.Do (fun conn -> ignore <| withOpenConn conn deleteAllUltrasonics
+                           withOpenConn conn insertAllUltrasonics)
+
