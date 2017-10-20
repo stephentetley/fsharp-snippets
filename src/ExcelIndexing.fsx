43baa38acfa74532e35ac0643017a89a85743d3c
stephentetley <stephen.tetley@gmail.com>
2017-10-20 22:20:00 +0100

    Added an Excel output example that writes to directly addressed cells.

---
+++ src/ExcelIndexing.fsx
@@ -0,0 +1,89 @@
+﻿#I @"..\packages\ExcelProvider.0.8.2\lib"
+#r "ExcelProvider.dll"
+open FSharp.ExcelProvider
+
+
+#r "Microsoft.Office.Interop.Excel"
+open Microsoft.Office.Interop
+
+
+type InputTable = ExcelFile< @"G:\work\Projects\routers\Batched-site-lists.xlsx",
+                            SheetName = "Site_List",
+                            ForceString = true >
+
+type InputRow = InputTable.Row
+
+let templatepath = @"G:\work\Projects\routers\Router Commission Sample TEMPLATE.xlsx"
+
+let safeName (input:string) : string = 
+    let bads = ['\\'; '/'; ':'; '#']
+    List.fold (fun s c -> s.Replace(c,'_')) input bads
+
+// TODO output to batches...
+let outfileName (sitename:string) (ipaddr:string) : string =
+    let s1 = safeName sitename
+    sprintf @"G:\work\Projects\routers\output\Router Commission %s %s.xlsx" s1 (safeName ipaddr)
+
+let outpath = outfileName @"KELDGATE/NO 3 WTW" "10.64.0.99"
+
+
+let saveWorkbook (workbook:Excel.Workbook) (filename:string)  : unit =
+    let app:Excel.Application = workbook.Application
+    // To disable overwrite alert
+    app.DisplayAlerts <- false
+    workbook.SaveAs(Filename = filename)
+    app.DisplayAlerts <- true
+    workbook.Close(SaveChanges = false)
+
+let writeCell (sheet:Excel.Worksheet) (cellindex:string) (value:string) : unit = 
+    sheet.Cells.Range(cellindex).Value2 <- value
+
+let writeKnownCells (sheet:Excel.Worksheet) (pairs:(string*string) list) : unit = 
+    List.iter (fun (a,b) -> writeCell sheet a b) pairs
+
+let makeCellInserts (rowi:InputRow) : (string*string) list =
+    [ ("B2", rowi.``AI2 Name`` )
+    ; ("D2", rowi.``SAI Number``)
+    ; ("B11", rowi.``Serial Number``)
+    ; ("B13", rowi.``Router IP Address``)
+
+    ]
+
+
+// Note filename should include number suffix e.g. (5) if specified...
+let processRow (app:Excel.Application) (rowi:InputRow) : unit = 
+    let outpath = outfileName rowi.``Colloquial name`` rowi.``Router IP Address``
+    let inserts = makeCellInserts rowi
+    let workbook : Excel.Workbook = app.Workbooks.Open(templatepath)
+    // let workbook = app.Workbooks.Item("data1.xlsx")
+    let worksheet = workbook.Sheets.["Proforma"] :?> Excel.Worksheet
+    writeKnownCells worksheet inserts
+    saveWorkbook workbook outpath
+
+
+let test01 () = 
+    // Run Excel as a visible application
+    let app = new Excel.ApplicationClass(Visible = true) 
+    let workbook : Excel.Workbook = app.Workbooks.Open(templatepath)
+    // let workbook = app.Workbooks.Item("data1.xlsx")
+    let worksheet = workbook.Sheets.["Proforma"] :?> Excel.Worksheet
+    writeKnownCells worksheet [ ("B2", "KELDGATE/NO 3 WTW")
+                              ; ("D2", "SAI00163740")
+                              ]
+    saveWorkbook workbook outpath
+    app.Quit()
+
+let test02 () = 
+    let file = new InputTable()
+    for (rowi:InputRow) in file.Data do
+       printfn "%s, %s, %s" rowi.``SAI Number`` rowi.``Serial Number`` rowi.``AI2 Name``
+
+let main () = 
+    let file = new InputTable()
+    let app = new Excel.ApplicationClass(Visible = true) 
+    for (rowi:InputRow) in file.Data do
+        match rowi.``AI2 Name`` with
+        | null -> printfn "<finished>"
+        | _ -> printfn "Processing %s..." rowi.``AI2 Name``
+               processRow app rowi
+    app.Quit()
