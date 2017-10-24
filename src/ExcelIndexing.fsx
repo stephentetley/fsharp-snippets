d76bf2ae84fad241bffd23a23801d1816c2dde54
stephentetley <stephen.tetley@gmail.com>
2017-10-24 23:13:24 +0100

    Added Excel sample that writes to cells referenced by index.

--- src/ExcelIndexing.fsx
+++ src/ExcelIndexing.fsx
@@ -8,8 +8,8 @@ open Microsoft.Office.Interop
 
 
 type InputTable = ExcelFile< @"G:\work\Projects\routers\Batched-site-lists.xlsx",
-                            SheetName = "Site_List",
-                            ForceString = true >
+                             SheetName = "Site_List",
+                             ForceString = true >
 
 type InputRow = InputTable.Row
 
@@ -19,15 +19,20 @@ let safeName (input:string) : string =
     let bads = ['\\'; '/'; ':'; '#']
     List.fold (fun s c -> s.Replace(c,'_')) input bads
 
-// TODO output to batches...
-let outfileName (sitename:string) (ipaddr:string) : string =
+let outfileName (batchname:string) (sitename:string) (ipaddr:string) : string =
+    let s0 = safeName batchname
     let s1 = safeName sitename
-    sprintf @"G:\work\Projects\routers\output\Router Commission %s %s.xlsx" s1 (safeName ipaddr)
+    sprintf @"G:\work\Projects\routers\output\%s\Router Commission %s %s.xlsx" s0 s1 (safeName ipaddr)
 
-let outpath = outfileName @"KELDGATE/NO 3 WTW" "10.64.0.99"
+
 
 
 let saveWorkbook (workbook:Excel.Workbook) (filename:string)  : unit =
+    let folderName = System.IO.Path.GetDirectoryName(filename)
+    if not <| System.IO.Directory.Exists (folderName) then
+        ignore <| System.IO.Directory.CreateDirectory folderName
+    else ()
+
     let app:Excel.Application = workbook.Application
     // To disable overwrite alert
     app.DisplayAlerts <- false
@@ -44,15 +49,20 @@ let writeKnownCells (sheet:Excel.Worksheet) (pairs:(string*string) list) : unit 
 let makeCellInserts (rowi:InputRow) : (string*string) list =
     [ ("B2", rowi.``AI2 Name`` )
     ; ("D2", rowi.``SAI Number``)
+    ; ("B4", rowi.``Site Address``)
+    ; ("B5", rowi.``Post Code``)
+    ; ("D7", rowi.``ASDL Provider``)
+    ; ("B9", rowi.``ASDL Line Number``)
     ; ("B11", rowi.``Serial Number``)
     ; ("B13", rowi.``Router IP Address``)
-
+    ; ("B19", rowi.``Paired with``)
+    ; ("D19", rowi.``Paired with I.P``)
     ]
 
 
 // Note filename should include number suffix e.g. (5) if specified...
-let processRow (app:Excel.Application) (rowi:InputRow) : unit = 
-    let outpath = outfileName rowi.``Colloquial name`` rowi.``Router IP Address``
+let processInputLine (app:Excel.Application) (rowi:InputRow) : unit = 
+    let outpath = outfileName rowi.``Batch Number`` rowi.``Colloquial name`` rowi.``Router IP Address``
     let inserts = makeCellInserts rowi
     let workbook : Excel.Workbook = app.Workbooks.Open(templatepath)
     // let workbook = app.Workbooks.Item("data1.xlsx")
@@ -62,6 +72,7 @@ let processRow (app:Excel.Application) (rowi:InputRow) : unit =
 
 
 let test01 () = 
+    let outpath = outfileName "Batch1" @"KELDGATE/NO 3 WTW" "10.64.0.99"
     // Run Excel as a visible application
     let app = new Excel.ApplicationClass(Visible = true) 
     let workbook : Excel.Workbook = app.Workbooks.Open(templatepath)
@@ -71,12 +82,13 @@ let test01 () =
                               ; ("D2", "SAI00163740")
                               ]
     saveWorkbook workbook outpath
+    workbook.Close(SaveChanges = false)
     app.Quit()
 
 let test02 () = 
     let file = new InputTable()
     for (rowi:InputRow) in file.Data do
-       printfn "%s, %s, %s" rowi.``SAI Number`` rowi.``Serial Number`` rowi.``AI2 Name``
+       printfn "%s, %s, %s" rowi.``Colloquial name`` rowi.``Serial Number`` rowi.``AI2 Name``
 
 let main () = 
     let file = new InputTable()
@@ -84,6 +96,6 @@ let main () =
     for (rowi:InputRow) in file.Data do
         match rowi.``AI2 Name`` with
         | null -> printfn "<finished>"
-        | _ -> printfn "Processing %s..." rowi.``AI2 Name``
-               processRow app rowi
+        | _ -> printfn "Processing %s..." rowi.``Colloquial name``
+               processInputLine app rowi
     app.Quit()
---
+++ src/ExcelPairwise.fsx
@@ -0,0 +1,90 @@
+﻿#r "Microsoft.Office.Interop.Excel"
+open Microsoft.Office.Interop
+
+open System
+
+
+// TODO - this needs testing, but maybe it is not important...
+let columnName (i:int) : string = 
+    let rec work n ac = 
+        if n > 0 then
+            let remainder = n % 26
+            if remainder = 0 then
+                work ((n/26)-1) ('Z' :: ac)
+            else 
+                let ch = char (64+remainder)
+                work (n/26) (ch :: ac) 
+        else ac
+    work i [] |> String.Concat
+
+let cellIndex (col:string) (row:int) : string = 
+    sprintf "%s%d" col row
+
+type Item = { Name : string; 
+              SerialNumber : string;
+              IPAddress: string;
+              ADSLNumber: string;
+              PairedWith : string;
+              PairedWithIP : string }
+
+let inputPath = @"G:\work\Projects\routers\IP_Subnets.xlsx"
+let outputPath = @"G:\work\Projects\routers\IP_Subnets_merged.xlsx"
+
+
+let saveAndCloseWorkbook (workbook:Excel.Workbook) (filename:string)  : unit =
+    let app:Excel.Application = workbook.Application
+    // To disable overwrite alert
+    app.DisplayAlerts <- false
+    workbook.SaveAs(Filename = filename)
+    app.DisplayAlerts <- true
+    workbook.Close(SaveChanges = false)
+
+let writeRow (sheet:Excel.Worksheet) (rowindex:int) (value:Item) : unit = 
+    sheet.Cells.Range(cellIndex "A" rowindex).Value2 <- value.Name
+    sheet.Cells.Range(cellIndex "B" rowindex).Value2 <- value.SerialNumber
+    sheet.Cells.Range(cellIndex "C" rowindex).Value2 <- value.IPAddress
+    sheet.Cells.Range(cellIndex "D" rowindex).Value2 <- value.ADSLNumber
+    sheet.Cells.Range(cellIndex "E" rowindex).Value2 <- value.PairedWith
+    sheet.Cells.Range(cellIndex "F" rowindex).Value2 <- value.PairedWithIP
+
+let test01 () = 
+    // Run Excel as a visible application
+    let app = new Excel.ApplicationClass(Visible = true) 
+    let inputWorkbook : Excel.Workbook = app.Workbooks.Open(inputPath)
+    let inputWorksheet = inputWorkbook.Sheets.["IP_Subnets"] :?> Excel.Worksheet
+    for i in 2 .. 2 .. 1000 do
+        let ix = sprintf "A%d" i
+        printf "%i: %s\n" i ((inputWorksheet.Cells.Range(ix).Value2) :?> string)
+    app.Quit()
+
+let main () = 
+    // Run Excel as a visible application
+    let app = new Excel.ApplicationClass(Visible = true) 
+    let inputWorkbook : Excel.Workbook = app.Workbooks.Open(inputPath)
+    let inputWorksheet = inputWorkbook.Sheets.["IP_Subnets"] :?> Excel.Worksheet
+    let outputWorkbook : Excel.Workbook = app.Workbooks.Add()
+    let outputWorksheet = outputWorkbook.Sheets.[1] :?> Excel.Worksheet
+    for i in 2 .. 2 .. 1000 do
+        let item1 = 
+            { Name = inputWorksheet.Cells.Range(cellIndex "A" i).Value2 :?> string; 
+              SerialNumber = inputWorksheet.Cells.Range(cellIndex "B" i).Value2 :?> string; 
+              IPAddress = inputWorksheet.Cells.Range(cellIndex "D" i).Value2 :?> string;
+              ADSLNumber = inputWorksheet.Cells.Range(cellIndex "E" i).Value2 :?> string;
+              PairedWith = inputWorksheet.Cells.Range(cellIndex "A" (i+1)).Value2 :?> string;
+              PairedWithIP = inputWorksheet.Cells.Range(cellIndex "D" (i+1)).Value2 :?> string; }
+
+        printf "%i: %s ==> %s \n" i item1.Name item1.PairedWith
+        let item2 = 
+            { Name = inputWorksheet.Cells.Range(cellIndex "A" (i+1)).Value2 :?> string; 
+              SerialNumber = inputWorksheet.Cells.Range(cellIndex "B" (i+1)).Value2 :?> string; 
+              IPAddress = inputWorksheet.Cells.Range(cellIndex "D" (i+1)).Value2 :?> string;
+              ADSLNumber = inputWorksheet.Cells.Range(cellIndex "E" (i+1)).Value2 :?> string;
+              PairedWith = inputWorksheet.Cells.Range(cellIndex "A" i).Value2 :?> string;
+              PairedWithIP = inputWorksheet.Cells.Range(cellIndex "D" i).Value2 :?> string; }
+
+        printf "%i: %s ==> %s \n" (i+1) item2.Name item2.PairedWith
+        writeRow outputWorksheet i item1
+        writeRow outputWorksheet (i+1) item2
+    saveAndCloseWorkbook outputWorkbook outputPath
+    inputWorkbook.Close(SaveChanges = false)
+    app.Quit()
