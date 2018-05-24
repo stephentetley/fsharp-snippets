e2920b666ea2fd16531b8e4c5d972ea932031bdd
stephentetley <stephen.tetley@gmail.com>
2018-05-24 16:25:08 +0100

    RTUDataExplore - work on interpreting the data with FParsec.

--- src/RTUDataExplore.fsx
+++ src/RTUDataExplore.fsx
@@ -36,13 +36,13 @@ open Microsoft.FSharp.Data.UnitSystems.SI.UnitNames
 #load @"SL\SQLiteConn.fs"
 #load @"SL\JsonExtractor.fs"
 #load @"SL\ScriptMonad.fs"
-#load @"SL\ExcelProviderHelper.fs"
+#load @"SL\CsvOutput.fs"
+#load @"SL\CsvUtils.fs"
 open SL.AnswerMonad
 open SL.SqlUtils
 open SL.SQLiteConn
 open SL.ScriptMonad
-open SL.ExcelProviderHelper
-
+open SL.CsvUtils
 
 type OsReport = 
     CsvProvider< 
@@ -55,7 +55,7 @@ type OsReportRow = OsReport.Row
 
 type PointReport = 
     CsvProvider< 
-        Sample = @"G:\work\Projects\events2\data\rtu-data\RTS-WW-all-points.temp.csv",
+        Sample = @"G:\work\Projects\events2\data\rtu-data\rtu-tad-all-points.trim.csv",
         HasHeaders = true,
         IgnoreErrors = true >
 
@@ -127,7 +127,9 @@ let makeSqlPointDataRow (csvRow:PointReportRow) : unit =
     row.OdName <- trimOption << nullToOption <| csvRow.``OD name``
     row.PointFunction <- trimOption << nullToOption <| csvRow.Function
     row.RecordingInfo <- trimOption << nullToOption <| csvRow.``Recording & timebase (on DG: on archive)``
-
+    row.ScalingOrMnemonics <- trimOption << nullToOption <| csvRow.``Scaling (raw=scaled)/mnemonics``
+    row.AlarmParameters <- trimOption << nullToOption <| csvRow.``Alarm parameters``
+    row.ControlPicAlarmPic <- trimOption << nullToOption <| csvRow.``Ctrl pic  Alarm pic``
 
 
 
@@ -170,20 +172,83 @@ let setupDB () : unit =
     dbAddOsRecords ()
     dbAddPointRecords () 
 
-let dti (input:string) : option<char * char * int> = 
-    let parseDTI: Parser<char * char * int, unit> = 
-        tuple3 (anyChar .>> spaces) (anyChar .>> spaces) pint32
+type PointDerived = Real | Derived | Calculated
+
+/// S A I
+type PointType = Analogue | Status | Integrated
+
+let pointDerived : Parser<PointDerived,unit> = 
+    (pchar 'D' |>> fun _ -> Derived)        <|> 
+    (pchar 'R' |>> fun _ -> Real)           <|>
+    (pchar 'C' |>> fun _ -> Calculated)
+
+let pointType : Parser<PointType,unit> = 
+    (pchar 'S' |>> fun _ -> Status)   <|> 
+    (pchar 'A' |>> fun _ -> Analogue)   <|>
+    (pchar 'I' |>> fun _ -> Integrated)
+
+let parseDTI : Parser<PointDerived * PointType * int, unit> = 
+    tuple3 (pointDerived .>> spaces) (pointType .>> spaces) pint32
+
+let (|||) (f:char -> bool) (g:char -> bool) : char -> bool = 
+    fun ch -> f ch || g ch
+
+let mnemonic1 : Parser<string,unit> = 
+    many1Satisfy2 (System.Char.IsUpper) 
+                    (System.Char.IsLetterOrDigit ||| (fun c -> c='_')) 
+
+let mnemonics : Parser<string list, unit> = 
+    sepBy1 mnemonic1 spaces1
+
+let pdecimal = pfloat |>> decimal
+
+type Scaling = int * decimal
+
+let scaling1 : Parser<Scaling,unit> = 
+    tuple2 (pint32 .>> pchar '=') pdecimal
+
+let scalings : Parser<Scaling list,unit> = 
+    sepBy1 scaling1 (pchar ',' .>> spaces1)
+
+type AlarmLocation = OS | DG
+
+let alarmLocation : Parser<AlarmLocation, unit> = 
+    (pstring "OS" |>> fun _ -> OS)  <|>
+    (pstring "DG" |>> fun _ -> DG) 
+
+// TO COMPLETE!
+let alarmParameters : Parser<AlarmLocation * string, unit> = 
+    tuple2 (alarmLocation .>> spaces1) (restOfLine false)
+
+let readDTI (input:string) : option<PointDerived * PointType * int> = 
     match run parseDTI input with
     | Success(a,_,_) -> Some a
-    | Failure(s,_,_) -> printfn "%s" s; None
+    | Failure(s,_,_) -> failwithf "dti - %s" input //  printfn "%s" s; None
 
+let runrun (p:Parser<'a,unit>) (input:string) : option<'a> = 
+    match run p input with
+    | Success(a,_,_) -> Some a
+    | Failure(s,_,_) -> failwithf "runrun - %s" input //  printfn "%s" s; None
 
-let temp01 () = dti "C    A    1"
+let temp01 () = readDTI "C    A    1"
 
 let temp02 () : unit = 
     query { 
         for c in sqlCtx.Main.PointData do 
         select (c.TypeAndIx) 
-        } |> Seq.iter (Option.iter (printfn "%A" << dti))
+        } |> Seq.iter (Option.iter (printfn "%A" << readDTI))
 
 
+let temp03 () = 
+    let input  = @"G:\work\Projects\events2\data\rtu-data\rtu-tad-all-points.tab.csv"
+    let output = @"G:\work\Projects\events2\data\rtu-data\rtu-tad-all-points.trim.csv"
+    let options = 
+        { InputSeparator = "\t"
+          InputHasHeaders = true
+          OutputSeparator = "," }
+    trimCsvFile options input output
+
+let temp04 () = runrun mnemonics "ISOLATED POWER_ON"
+let temp05 () = runrun scalings "2000=-20.0000, 16000=85.0000"
+
+let temp06 () = runrun alarmParameters "DG OFF/E ON/E TDB=60s"
